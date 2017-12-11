e8c3265ad1d34a9670f4439980a735925d2f8c16
Smartindo_31 <Smartindo_31@Smartindo_31-PC>
2017-12-11 17:05:08 +0700

    1112

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -9,71 +9,84 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;import java.util.stream.Collector;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityManagerFactory;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 public class GreetingController {
 
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
-                            String.format(template, name));
-    }
-    
-    @RequestMapping("/data")
-    public List<String> dataNegara (
-    		@RequestParam ("pre") String prefix){
-    	List <String> data = new ArrayList<>();
-    	data.add("Indonesia");
-    	data.add("Malaysia");
-    	data.add("Brunei");
-    	data.add("Timor  Leste");
+	// autowired > utk ambil object di server utk di assign ke object greeting x
+	@Autowired
+	private Greeting x;
 
-    	
-    	return data.stream().filter(line -> line.startsWith(prefix)).collect(Collectors.toList());
-    }
-    
-    @RequestMapping("/countries")
-    public String getCountries () throws IOException {
-//    	URL url = new URL ("http://www.webservicex.net/country.asmx/GetCountries");
-//    	URLConnection connection = url.openConnection();
-//    	connection.setDoOutput(true);
-    	
-    	URL url = new URL ("http://www.webservicex.net/globalweather.asmx/GetCitiesByCountry?CountryName=string");
-    	URLConnection connection = url.openConnection();
-//    	connection.setDoOutput(true);
-//    	connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-//    	connection.setRequestProperty("Content-Length", "0");
-    	    	
-    	InputStream stream = url.openConnection().getInputStream();
-    	InputStreamReader reader = new InputStreamReader(stream);
-    	BufferedReader buffer = new BufferedReader(reader);
-    	
-    	
-    	String line;
-    	StringBuilder builder = new StringBuilder();
-    	
-    	while ((line = buffer.readLine()) != null) {
-    		builder.append(line);
-    	}
-    	
-    	return builder.toString();
-//    	return new BufferedReader(new InputStreamReader(stream)).lines().collect(Collectors.joining("\n"));
-    	
+	@Autowired
+	private EntityManagerFactory em;
+	
+	@RequestMapping ("/actors")
+	public List<Actor> allActor (){
+		return em.createEntityManager().createQuery("from Actor").getResultList();
+	}
+	
+	@RequestMapping("/greeting")
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return x;
+		// return new Greeting(counter.incrementAndGet(),
+		// String.format(template, name));
+	}
 
-    }
+	@RequestMapping("/data")
+	public List<String> dataNegara(@RequestParam("pre") String prefix) {
+		List<String> data = new ArrayList<>();
+		data.add("Indonesia");
+		data.add("Malaysia");
+		data.add("Brunei");
+		data.add("Timor  Leste");
+
+		return data.stream().filter(line -> line.startsWith(prefix)).collect(Collectors.toList());
+	}
+
+	@RequestMapping("/countries")
+	public String getCountries() throws IOException {
+		// URL url = new URL ("http://www.webservicex.net/country.asmx/GetCountries");
+		// URLConnection connection = url.openConnection();
+		// connection.setDoOutput(true);
+
+		URL url = new URL("http://www.webservicex.net/globalweather.asmx/GetCitiesByCountry?CountryName=string");
+		URLConnection connection = url.openConnection();
+		// connection.setDoOutput(true);
+		// connection.setRequestProperty("Content-Type",
+		// "application/x-www-form-urlencoded");
+		// connection.setRequestProperty("Content-Length", "0");
+
+		InputStream stream = url.openConnection().getInputStream();
+		InputStreamReader reader = new InputStreamReader(stream);
+		BufferedReader buffer = new BufferedReader(reader);
+
+		String line;
+		StringBuilder builder = new StringBuilder();
+
+		while ((line = buffer.readLine()) != null) {
+			builder.append(line);
+		}
+
+		return builder.toString();
+		// return new BufferedReader(new
+		// InputStreamReader(stream)).lines().collect(Collectors.joining("\n"));
+
+	}
 
 }
-
 
 // SOAP & REST
 // REST >> HTTP GET & HTTP POST
